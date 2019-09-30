e54bc907ab187e81f24e1039a40ece31db3eb597
YongLAGCC <1291001957.yz@gmail.com>
2019-09-29 20:43:02 -0400

    done hw2

--- PS2/P1.js
+++ PS2/P1.js
@@ -1,23 +1,18 @@
 
-const fibonacci = function* (n, n1 = 0, n2 = 1){
-    if (n === 0) {
-        return n1;
-    }
-    yield n1;
-    yield *fibonacci(n-1, n2, n1 + n2);
+const fib = function*(n, n1 = 0, n2 = 1){
+
+    if (n === 0) { return n1; }
+    yield n1;  yield *fib(n-1, n2, n1 + n2);
 }
 
-const fibs = n => fibonacci(20)
+const fibs = n => fib(20)
 myFibs = fibs()
 
 const isEven= function* (){
     let count = 0;
     while (count < 6) {
         temp = myFibs.next().value
-        if(temp % 2 == 0){
-            yield temp;
-        }
-
+        if(temp % 2 == 0){  yield temp;  }
     }
 }
 
